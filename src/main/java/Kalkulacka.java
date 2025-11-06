1e597204bebb99f0abcbd2e679b7f9d83bb2f33d
Jan Sima <simahonza@seznam.cz>
2025-11-06 20:31:46 +0100

    Pridani kalkulacky

---
+++ src/main/java/Kalkulacka.java
@@ -0,0 +1,27 @@
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class Kalkulacka {
+
+    private int prvCislo;
+    private int druCislo;
+    private int vypoctenyVysledek;
+
+    @Given("Uzivatel ma zadany dve cisla {int} a {int}")
+    public void uzivatelMaZadanyDveCislaA(int prvniCislo, int druheCislo) {
+        prvCislo = prvniCislo;
+        druCislo = druheCislo;
+    }
+
+    @When("Uzivatel secte tato dve cisla")
+    public void uzivatelSecteTatoDveCisla() {
+        vypoctenyVysledek = prvCislo + druCislo;
+    }
+
+    @Then("Uzivatel vidi na kalkulacce vysledek {int}")
+    public void uzivatelVidiNaKalkulacceVysledek(int predpokladanyVysledek) {
+        assertEquals(predpokladanyVysledek, vypoctenyVysledek);
+    }
+}
