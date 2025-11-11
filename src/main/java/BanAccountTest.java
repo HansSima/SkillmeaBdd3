f9f19a4f618522c2c896aee7ad5d4a50fb71df0e
Jan Sima <simahonza@seznam.cz>
2025-11-11 19:01:02 +0100

    Pridani dalsich kroku bankovniho uctu

--- src/main/java/BanAccountTest.java
+++ src/main/java/BanAccountTest.java
@@ -1,22 +1,32 @@
 import io.cucumber.java.PendingException;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import product.BankAccount;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class BanAccountTest {
-
+    BankAccount prvniBankovniUcet;
 
     @Given("Uzivatel ma bankovni ucet s {int} kc")
     public void uzivatelMaBankovniUcetSKc(int pocatecniZustatekNaUctu) {
-        BankAccount prvniBankovniUcet = new BankAccount();
-
+       prvniBankovniUcet = new BankAccount(pocatecniZustatekNaUctu);
     }
 
     @Then("Na uzivatelskem uctu je {int} kc")
     public void naUzivatelskemUctuJeKc(int predpokladanyZustatekNaUctu) {
-
-
+        assertEquals(predpokladanyZustatekNaUctu, prvniBankovniUcet.getAccountBalance(), "Test spadl");
     }
 
 
+    @When("Uzivatel si na tento ucet prevede {int} kc")
+    public void uzivatelSiNaTentoUcetPrevedeKc(int castkaKPrevodu) {
+
+    }
+
+    @And("Uzivatel si z tohoto uctu vybere {int} kc")
+    public void uzivatelSiZTohotoUctuVybereKc(int castkaKVyberu) {
+
+    }
 }
--- src/main/resources/Bank.feature
+++ src/main/resources/Bank.feature
@@ -3,3 +3,9 @@ Feature: Bank
   Scenario: Zustatek na uctu
     Given Uzivatel ma bankovni ucet s 1000 kc
     Then Na uzivatelskem uctu je 1000 kc
+
+  Scenario: Ucet prevod a vyber penez
+    Given Uzivatel ma bankovni ucet s 1500 kc
+    When Uzivatel si na tento ucet prevede 1000 kc
+    And Uzivatel si z tohoto uctu vybere 2000 kc
+    Then Na uzivatelskem uctu je 500 kc
